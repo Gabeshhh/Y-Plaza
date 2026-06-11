79ba3a3811af65cb0368ae3dfc5b31ff0e08e89d
Gabeshh <gabriel.laugier@ynov.com>
2026-06-11 18:38:31 +0200

    New: fichier RequestJava

---
+++ backend/src/main/java/com/yplaza/dto/request/RegisterRequest.java
@@ -0,0 +1,32 @@
+package main.java.com.yplaza.dto.request;
+
+import com.yplaza.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+/**
+ * Formulaire d'inscription au site avec certaines regles
+ */
+
+@Data
+public class RegisterRequest {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    private String password;
+
+    private String phone;
+    private Role role = Role.CLIENT;
+    private Long agencyId;
+}
