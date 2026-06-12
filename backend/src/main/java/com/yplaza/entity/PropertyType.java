702d2a5db94273acc878fcb7f6bffdd0d1cb77d1
Gabeshh <gabriel.laugier@ynov.com>
2026-06-12 15:15:17 +0200

    push nex ficheir

---
+++ backend/src/main/java/com/yplaza/entity/PropertyType.java
@@ -0,0 +1,13 @@
+package main.java.com.yplaza.entity;
+
+/** Type de bien immobilier */
+public enum PropertyType {
+    APPARTEMENT,
+    MAISON,
+    VILLA,
+    STUDIO,
+    BUREAU,
+    LOCAL_COMMERCIAL,
+    TERRAIN,
+    ENTREPOT
+}
